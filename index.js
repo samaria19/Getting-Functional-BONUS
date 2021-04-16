fe2f602f7a2420d0f5042f2fb99a6b9666e40935
Samaria Smothers <>
2021-04-16 17:44:41 +0000

    1

--- index.js
+++ index.js
@@ -13,10 +13,17 @@ numbers and know how to access the balance property
 in the object.
 
 */
+//1.how do you find an averge for a set of number
+//2,where can you find the balance for each customers
+//3.where can you store the totle sum of balances
+//4.can you make this work for a data set of infinite length
 
 function averageBalance(array){
   //CODE BELOW HERE
-
+  var sum = 0
+for(var i=0; i<= array.length - 1; i++ ){
+  (sum += array[i].balance / customers.length) 
+}
 
 
 
